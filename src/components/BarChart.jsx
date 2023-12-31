3bbd75e9405c072612908f988bb21f4e37fe1958
TropicolX <68024640+TropicolX@users.noreply.github.com>
2023-12-31 14:22:36 +0000

    refactor: Update height attribute in BarChart component
    Update height attribute in BarChart component to remove unused parameter

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -3,9 +3,9 @@ import * as d3 from "d3";
 
 const Chart = () => {
   const chartRef = useRef();
-  const data = [130, 200, 170, 140, 130, 250, 160];
-
+  
   useEffect(() => {
+    const data = [130, 200, 170, 140, 130, 250, 160];
     const svgHeight = 300;
     const svgWidth = 450;
     const barWidth = 60;
@@ -20,7 +20,7 @@ const Chart = () => {
       .data(data)
       .join("rect")
       .attr("width", barWidth)
-      .attr("height", (barHeight, i) => barHeight)
+      .attr("height", (barHeight) => barHeight)
       .attr("fill", "skyblue")
       .attr("x", (_, i) => i * (barWidth + 5))
       .attr("y", (barHeight) => svgHeight - barHeight);
